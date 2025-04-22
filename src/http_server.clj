166c130666e90a996c4ea072ffa84c4608fed650
izpa <ispius.prime@gmail.com>
2025-04-22 16:19:05 +0300

    add routing

--- src/http_server.clj
+++ src/http_server.clj
@@ -2,21 +2,34 @@
   (:require
    [integrant.core :as ig]
    [org.httpkit.server :as hk-server]
-   [taoensso.timbre :as log]))
+   [reitit.ring :as ring]
+   [ring.middleware.defaults
+    :refer [wrap-defaults api-defaults]]
+   [taoensso.timbre :as log]
+   [ring.util.response :as response]))
 
-(defmethod ig/init-key ::handler [_ _]
-  #(do
-     (log/info "Receive http:" %)
-     {:status  200
-      :body "foo"
-      :headers {"Content-Type" "text/html"}}))
+(defmethod ig/init-key ::route-handler [_ routes]
+  (ring/ring-handler
+   (ring/router routes)))
 
-(defmethod ig/init-key ::server [_ {:keys [handler port]}]
+(defmethod response/resource-data :resource
+  [^java.net.URL url]
+  (let [resource (.openConnection url)
+        len (#'ring.util.response/connection-content-length resource)]
+    (when (pos? len)
+      {:content        (.getInputStream resource)
+       :content-length len
+       :last-modified  (#'ring.util.response/connection-last-modified resource)})))
+
+(defmethod ig/init-key ::server [_ {:keys [route-handler port]}]
   (log/info "Start http-server on port " port)
-  (hk-server/run-server handler {:port (->> port
-                                            str
-                                            (re-find  #"\d+")
-                                            Integer/parseInt)}))
+  (hk-server/run-server (wrap-defaults
+                         route-handler
+                         (assoc api-defaults :static {:resources "public"}))
+                        {:port (->> port
+                                    str
+                                    (re-find  #"\d+")
+                                    Integer/parseInt)}))
 
 (defmethod ig/halt-key! ::server [_ server]
   (log/info "Stopping server" server)
