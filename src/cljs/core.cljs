c6dc3953c7c071d65f61a1dfd1419728accbd878
izpa <ispius.prime@gmail.com>
2025-04-25 11:32:17 +0300

    fix pagination on frontend

--- src/cljs/core.cljs
+++ src/cljs/core.cljs
@@ -7,7 +7,6 @@
    [day8.re-frame.http-fx]
    [clojure.string :as str]))
 
-;; Initial state
 (rf/reg-event-db
  :initialize
  (fn [_ _]
@@ -17,25 +16,13 @@
     :articles-total 0
     :articles-filter ""
     :articles-page 1
-    :articles-per-page 10}))
+    :articles-per-page 10
+    :articles-page-input "1"}))
 
-;; Keywords logic
-(rf/reg-event-db
- :add-keyword
- (fn [db _]
-   (update db :keywords conj "")))
+(rf/reg-event-db :add-keyword (fn [db _] (update db :keywords conj "")))
+(rf/reg-event-db :remove-keyword (fn [db [_ idx]] (update db :keywords #(vec (concat (subvec % 0 idx) (subvec % (inc idx)))))))
+(rf/reg-event-db :update-keyword (fn [db [_ idx val]] (assoc-in db [:keywords idx] val)))
 
-(rf/reg-event-db
- :remove-keyword
- (fn [db [_ idx]]
-   (update db :keywords #(vec (concat (subvec % 0 idx) (subvec % (inc idx)))))))
-
-(rf/reg-event-db
- :update-keyword
- (fn [db [_ idx val]]
-   (assoc-in db [:keywords idx] val)))
-
-;; Find
 (rf/reg-event-fx
  :do-find
  (fn [{:keys [db]} _]
@@ -52,7 +39,8 @@
  :find-success
  (fn [db [_ result]]
    (let [normalize (fn [a]
-                     {:publication_name (:publication_name a)
+                     {:keyword (:keyword a)
+                      :publication_name (:publication_name a)
                       :cover_date (:cover_date a)
                       :creator (:creator a)
                       :doi (:doi a)})
@@ -60,44 +48,33 @@
          existed (mapv normalize (:existed result))]
      (assoc db :find-results {:new new :existed existed}))))
 
-;; Subscriptions
 (rf/reg-sub :keywords (fn [db _] (:keywords db)))
 (rf/reg-sub :find-results (fn [db _] (:find-results db)))
 (rf/reg-sub :articles-list (fn [db _] (:articles-list db)))
 (rf/reg-sub :articles-total (fn [db _] (:articles-total db)))
 (rf/reg-sub :articles-filter (fn [db _] (:articles-filter db)))
 (rf/reg-sub :articles-page (fn [db _] (:articles-page db)))
+(rf/reg-sub :articles-page-input (fn [db _] (:articles-page-input db)))
 (rf/reg-sub :articles-per-page (fn [db _] (:articles-per-page db)))
 
-;; Filter / Pagination events
-(rf/reg-event-db
- :update-articles-filter
- (fn [db [_ val]]
-   (assoc db :articles-filter val)))
+(rf/reg-event-db :update-articles-filter (fn [db [_ val]] (assoc db :articles-filter val)))
+(rf/reg-event-db :update-articles-per-page (fn [db [_ val]] (assoc db :articles-per-page val)))
+(rf/reg-event-db :update-articles-page-input (fn [db [_ val]] (assoc db :articles-page-input val)))
 
-(rf/reg-event-fx
- :update-articles-page
- (fn [{:keys [db]} [_ val]]
-   {:db (assoc db :articles-page val)
-    :dispatch [:fetch-articles]}))
-
-(rf/reg-event-fx
- :update-articles-per-page
- (fn [{:keys [db]} [_ val]]
-   {:db (assoc db :articles-per-page val)
-    :dispatch [:fetch-articles]}))
-
-;; Fetch articles
 (rf/reg-event-fx
  :fetch-articles
- (fn [{:keys [db]} _]
-   (let [{:keys [articles-filter articles-page articles-per-page]} db
-         offset (* (max 0 (dec articles-page)) articles-per-page)]
-     {:http-xhrio {:method :get
-                   :uri (str "/articles"
-                             "?q=" (js/encodeURIComponent articles-filter)
-                             "&offset=" offset
-                             "&limit=" articles-per-page)
+ (fn [{:keys [db]} [_ page]]
+   (let [current-page (:articles-page db)
+         new-page (or page current-page)
+         per-page (:articles-per-page db)
+         offset (* (dec new-page) per-page)
+         query (:articles-filter db)]
+     {:db (assoc db 
+                 :articles-page new-page
+                 :articles-page-input (str new-page))
+      :http-xhrio {:method :get
+                   :uri (str "/articles?q=" (js/encodeURIComponent query)
+                             "&offset=" offset "&limit=" per-page)
                    :response-format (ajax.core/json-response-format {:keywords? true})
                    :on-success [:articles-success]}})))
 
@@ -105,17 +82,65 @@
  :articles-success
  (fn [db [_ result]]
    (let [{:keys [articles total]} result
-         simplify-keys (fn [article]
-                         {:title (:articles/publication_name article)
-                          :author (:articles/creator article)
-                          :date (:articles/cover_date article)
-                          :doi (:articles/doi article)})
-         simplified (mapv simplify-keys articles)]
-     (assoc db
-            :articles-list simplified
-            :articles-total total))))
+         simplify (fn [a] {:title (:articles/publication_name a)
+                           :author (:articles/creator a)
+                           :date (:articles/cover_date a)
+                           :doi (:articles/doi a)})]
+     (assoc db :articles-list (mapv simplify articles)
+               :articles-total total))))
 
-;; UI
+(rf/reg-event-fx
+ :change-page
+ (fn [{:keys [db]} [_ new-page]]
+   (let [per-page (:articles-per-page db)
+         total (:articles-total db)
+         max-page (max 1 (js/Math.ceil (/ total per-page)))
+         validated-page (cond
+                         (string? new-page) (let [parsed (js/parseInt new-page)]
+                                             (if (js/isNaN parsed)
+                                               (:articles-page db)
+                                               (max 1 (min parsed max-page))))
+                         (number? new-page) (max 1 (min new-page max-page))
+                         :else (:articles-page db))]
+     {:dispatch [:fetch-articles validated-page]})))
+
+(defn pagination-controls []
+  (let [total @(rf/subscribe [:articles-total])
+        current-page @(rf/subscribe [:articles-page])
+        per-page @(rf/subscribe [:articles-per-page])
+        page-input @(rf/subscribe [:articles-page-input])
+        max-page (max 1 (js/Math.ceil (/ total per-page)))]
+    [:div.pagination
+     [:button 
+      {:disabled (= current-page 1)
+       :on-click #(rf/dispatch [:change-page 1])}
+      "<<"]
+     [:button 
+      {:disabled (= current-page 1)
+       :on-click #(rf/dispatch [:change-page (dec current-page)])}
+      "<"]
+     [:span " Page "]
+     [:input 
+      {:type "number"
+       :min 1
+       :max max-page
+       :value page-input
+       :on-change #(rf/dispatch [:update-articles-page-input (.. % -target -value)])
+       :on-key-down #(when (= (.-key %) "Enter")
+                      (rf/dispatch [:change-page page-input]))}]
+     [:span (str " of " max-page)]
+     [:button 
+      {:on-click #(rf/dispatch [:change-page page-input])}
+      "Go"]
+     [:button 
+      {:disabled (= current-page max-page)
+       :on-click #(rf/dispatch [:change-page (inc current-page)])}
+      ">"]
+     [:button 
+      {:disabled (= current-page max-page)
+       :on-click #(rf/dispatch [:change-page max-page])}
+      ">>"]]))
+
 (defn keywords-input []
   (let [ks @(rf/subscribe [:keywords])]
     [:div
@@ -142,11 +167,11 @@
   (let [{:keys [new existed]} @(rf/subscribe [:find-results])]
     [:div
      (when (seq new)
-       [:details {:open true}
+       [:details
         [:summary (str "New articles (" (count new) ")")]
         [result-table new]])
      (when (seq existed)
-       [:details {:open false}
+       [:details
         [:summary (str "Existed articles (" (count existed) ")")]
         [result-table existed]])]))
 
@@ -160,30 +185,28 @@
       [:tr [:td title] [:td author] [:td date] [:td doi]])]])
 
 (defn article-section []
-  (let [f @(rf/subscribe [:articles-filter])
-        p @(rf/subscribe [:articles-page])
-        per @(rf/subscribe [:articles-per-page])
-        articles @(rf/subscribe [:articles-list])
-        total @(rf/subscribe [:articles-total])]
+  (let [articles @(rf/subscribe [:articles-list])
+        total @(rf/subscribe [:articles-total])
+        f @(rf/subscribe [:articles-filter])
+        per @(rf/subscribe [:articles-per-page])]
     [:div
      [:div
       [:label "Filter: "]
-      [:input {:type "text" :value f :on-change #(rf/dispatch [:update-articles-filter (.. % -target -value)])}]
+      [:input {:type "text"
+               :value f
+               :on-change #(rf/dispatch [:update-articles-filter (.. % -target -value)])}]
       [:label " Per page: "]
-      [:input {:type "number" :value per :on-change #(rf/dispatch [:update-articles-per-page (js/parseInt (.. % -target -value))])}]
-      [:button {:on-click #(rf/dispatch [:fetch-articles])} "Apply"]]
+      [:input {:type "number"
+               :min 1
+               :value per
+               :on-change #(rf/dispatch [:update-articles-per-page (js/parseInt (.. % -target -value))])}]
+      [:button {:on-click #(rf/dispatch [:fetch-articles 1])} "Apply"]]
      (cond
        (nil? articles) [:p "Loading..."]
        (empty? articles) [:p "No articles in db"]
        :else [article-table articles])
-     (when total
-       (let [max-page (max 1 (js/Math.ceil (/ total per)))]
-         [:div
-          [:button {:on-click #(rf/dispatch [:update-articles-page 1])} "<<"]
-          [:button {:on-click #(rf/dispatch [:update-articles-page (max 1 (dec p))])} "<"]
-          [:span (str " Page " p " of " max-page " ")]
-          [:button {:on-click #(rf/dispatch [:update-articles-page (min max-page (inc p))])} ">"]
-          [:button {:on-click #(rf/dispatch [:update-articles-page max-page])} ">>"]]))]))
+     (when (pos? total)
+       [pagination-controls])]))
 
 (defn app []
   [:div
@@ -198,5 +221,5 @@
 
 (defn init []
   (rf/dispatch-sync [:initialize])
-  (rf/dispatch [:fetch-articles])
+  (rf/dispatch [:fetch-articles nil])
   (mount))
